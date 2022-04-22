72c7a99342511a26ba84d3ac44e97a50f646a810
Eric Wu <eric.dianhao.wu@gmail.com>
2022-04-22 12:14:14 -0700

    initial commit

---
+++ lyric_converter.py
@@ -0,0 +1,44 @@
+from asyncore import read
+import json
+
+
+FILE_IN = "lyrics/Fearless--Mr. Perfectly Fine.txt"
+FILE_OUT = "lyrics-compiled/Fearless--Mr. Perfectly Fine.txt"
+
+
+def read_file():
+
+    words = []
+    sections = {}
+    currSection = None
+    with open(FILE_IN, 'r') as f:
+        for currLine, line in enumerate(f.readlines()):
+            currLine += 1
+            line = line.strip()
+            #skip blank lines
+            if line == "":
+                continue
+            #process song section labels:
+            if line.startswith("["):
+                if not line.endswith("]"):
+                    return f"Missing closing bracket expected on line {currLine}"
+                sectionName = line[1:-1]
+                if currSection is not None:
+                    sections[currSection].append(len(words))
+                sections[sectionName] = [len(words)]
+                currSection = sectionName
+                continue
+            #check for inappropriate use of dashes: only double dashes are allowed
+            if "-" in line.replace("--", ""):
+                return f"Bad use of dashes on line {currLine}"
+            
+            filteredLine = ""
+            for char in line:
+                if char.isalnum() or char == " " or char == "-":
+                    filteredLine += char
+            words += list(filteredLine.split(' '))
+            
+    
+    return words, sections
+        
+print(read_file())
---
+++ lyrics/README.md
@@ -0,0 +1,10 @@
+# File format for song lyrics
+
+All lyric files should be limited to only alphabetical characters and digits. 
+No parenthesis, punctuation, or quotes (These characters will be ignored by the translator).
+
+All blank lines will be ignored by the translator.
+
+We can indicate song sections by enclosing them in brackets and putting them on their own line. Song sections must be unique.
+
+Whitepsace separates words. A double dash (`--`) indicates a pause in the words when music is playing.
